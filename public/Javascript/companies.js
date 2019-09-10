49b547eb12e3ae3f16a350517c97b16c142e0a86
Pallav Jain <pallavjain409@gmail.com>
2019-09-10 09:59:01 +0530

    companies.hbs modified

--- public/Javascript/companies.js
+++ public/Javascript/companies.js
@@ -1,35 +1,39 @@
 $.ajax({
-    url: "/company/all",
-    type: 'GET',
-    dataType: 'json',
-    success: function (companies) {
-        getCompanies(companies)
-    }
+  url: "/company/all",
+  type: 'GET',
+  dataType: 'json',
+  success: function (companies) {
+      getCompanies(companies)
+  }
 });
 
 function getCompanies(companies) {
-    let str = ""
-    companies.forEach(company => {
-      if(!company.rating){
-        company.rating = "Not Rated Yet"
-      }
-      else{
-          company.rating = `${company.rating}/5`
-      }
-        str += `
-        <a href="/company/${company._id}" target = "_blank" class ="companyCard">
-            <div class="card" style="width: 18rem;">
-  <img class="card-img-top style="width: 5rem;"" src="${company.logo}" alt="${company.companyName}">
-  <div class="card-body">
-    <h5 class="card-title">${company.companyName}</h5>
-    <p class="card-text">${company.city}, ${company.country}</p>
-    <p class="card-text">${company.department}</p>
-    <p class="card-text">${company.rating}</p>
-      <button class="btn btn-primary">Read reviews</button>
-  </div>
+  console.log(companies);
+  let str = ""
+  companies.forEach(company => {
+    if(!company.rating){
+      company.rating = "Not Rated Yet"
+    }
+    else{
+        company.rating = `${company.rating}/5`
+    }
+      str += `
+      <div class = "col-6">
+      <a href="/company/${company._id}" target = "_blank" class ="companyCard">
+          <div class="card" style="width: 18rem;">
+<img class="card-img-top style="width: 5rem;"" src="${company.logo}" alt="${company.companyName}">
+<div class="card-body">
+  <h5 class="card-title">${company.companyName}</h5>
+  <p class="card-text">${company.country}</p>
+  <p class="card-text">${company.department}</p>
+  <p class="card-text">${company.rating}</p>
+    <button class="btn btn-primary">Read reviews</button>
+</div>
 </div>
 </a>
- ` });
-    
-    $('#company').html(str);
-}
+</div>
+` });
+console.log(str)
+
+  $('#company').html(str);
+} 
